36927b9432109ac0044940ea1ee888f868bffd27
SantiVigo <pereirasanti3008@gmail.com>
2025-05-12 09:46:06 +0200

    Delete programas/P4_Santiago.py

--- programas/P4_Santiago.py
+++
@@ -1,8 +0,0 @@
-from microbit import *
-
-pin6.set_analog_period(20)   #servo conectado ao pin6
-while True:
-    pin6.write_analog(180)     #o servo xira a 180º ao enviar un valor analóxico de 180
-    sleep(1000) #esperamos 1s
-    pin6.write_analog(1)
-    sleep(1000)
